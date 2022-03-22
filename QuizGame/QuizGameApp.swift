62d3e2b16f98a632f2888c2330f1a6738f912364
Korobskoy Roman <korobskoyra@gmail.com>
2022-03-22 17:28:00 +0300

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Roman Korobskoy on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
